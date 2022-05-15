7eee471ac80a4dd6effb7070e51109e71dee5ba7
Petar <PDMatsaliev20@codingburgas.bg>
2022-05-15 16:53:10 +0300

    Implement PVP and PVC functions

--- application/src/mainMenu.cpp
+++ application/src/mainMenu.cpp
@@ -3,32 +3,38 @@ namespace mainMenu{
     bool isInMainMenu = true;
     bool texturesLoaded = false;
 
-    Texture2D MenuButtonUnpressed;
-    Texture2D MenuButtonPressed;
     Texture2D RuleButtonUnpressed;
     Texture2D RuleButtonPressed;
     Texture2D QuitButtonUnpressed;
     Texture2D QuitButtonPressed;
+    Texture2D PVPButtonUnpressed;
+    Texture2D PVPButtonPressed;
+    Texture2D PVCButtonUnpressed;
+    Texture2D PVCButtonPressed;
 
     float buttonWidth;
     float buttonHeight;
     
     void loadTextures()
     {
-        MenuButtonUnpressed = LoadTexture("./Sprites/Begin-Button-Unpressed.png");
-        MenuButtonPressed = LoadTexture("./Sprites/Begin-Button-Pressed.png");
         RuleButtonUnpressed = LoadTexture("./Sprites/Rules-Button-Unpressed.png");
         RuleButtonPressed = LoadTexture("./Sprites/Rules-Button-Pressed.png");
         QuitButtonUnpressed = LoadTexture("./Sprites/Quit-Button-Unpressed.png");
         QuitButtonPressed = LoadTexture("./Sprites/Quit-Button-Pressed.png");
-        buttonWidth = MenuButtonPressed.width;
-        buttonHeight = MenuButtonPressed.height;
+        PVPButtonUnpressed = LoadTexture("./Sprites/PVP-button-Unpressed.png");
+        PVPButtonPressed = LoadTexture("./Sprites/PVP-button-Pressed.png");
+        PVCButtonPressed = LoadTexture("./Sprites/PVC-button-Pressed.png");
+        PVCButtonUnpressed = LoadTexture("./Sprites/PVC-button-Unpressed.png");
+        buttonWidth = PVPButtonPressed.width;
+        buttonHeight = PVPButtonPressed.height;
         texturesLoaded = true;
     }
     void unloadTextures()
     {
-        UnloadTexture(MenuButtonUnpressed);
-        UnloadTexture(MenuButtonPressed);
+        UnloadTexture(PVCButtonPressed);
+        UnloadTexture(PVCButtonUnpressed);
+        UnloadTexture(PVPButtonPressed);
+        UnloadTexture(PVPButtonUnpressed);
         UnloadTexture(RuleButtonUnpressed);
         UnloadTexture(RuleButtonPressed);
         UnloadTexture(QuitButtonUnpressed);
@@ -54,13 +60,13 @@ namespace mainMenu{
             Vector2 MousePos = {GetMousePosition().x, GetMousePosition().y};
 
             //Draw the menu button
-            DrawTexture(MenuButtonUnpressed, buttonPos.x, buttonPos.y, WHITE);
+            DrawTexture(PVPButtonUnpressed, buttonPos.x, buttonPos.y, WHITE);
             //Detect if the mouse is over the menu button
             if(CheckCollisionPointRec(MousePos, {buttonPos.x, buttonPos.y + 4, buttonWidth, buttonHeight}))
             {
                 SetMouseCursor(4);
                 DrawRectangle(buttonPos.x, buttonPos.y, buttonWidth, buttonHeight, RAYWHITE);
-                DrawTexture(MenuButtonPressed, buttonPos.x, buttonPos.y + 4, WHITE);
+                DrawTexture(PVPButtonPressed, buttonPos.x, buttonPos.y + 4, WHITE);
                 if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
                 {
                     unloadTextures();
@@ -70,13 +76,13 @@ namespace mainMenu{
             }
 
             //Draw the rules button
-            DrawTexture(RuleButtonUnpressed, buttonPos.x, buttonPos.y + 100, WHITE);
+            DrawTexture(PVCButtonUnpressed, buttonPos.x, buttonPos.y + 100, WHITE);
             //Detect if the mouse is over the rules button
             if(CheckCollisionPointRec(MousePos, {buttonPos.x, buttonPos.y + 104, buttonWidth, buttonHeight}))
             {
                 SetMouseCursor(4);
                 DrawRectangle(buttonPos.x, buttonPos.y + 100, buttonWidth, buttonHeight, RAYWHITE);
-                DrawTexture(RuleButtonPressed, buttonPos.x, buttonPos.y + 104, WHITE);
+                DrawTexture(PVCButtonPressed, buttonPos.x, buttonPos.y + 104, WHITE);
                 if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
                 {
                     unloadTextures();
@@ -85,14 +91,28 @@ namespace mainMenu{
                 }     
             }
 
-            //Draw the quit button
-            DrawTexture(QuitButtonUnpressed, buttonPos.x, buttonPos.y + 200, WHITE);
-            //Detect if the mouse is over the quit button
+            DrawTexture(RuleButtonUnpressed, buttonPos.x, buttonPos.y + 200, WHITE);
+
             if(CheckCollisionPointRec(MousePos, {buttonPos.x, buttonPos.y + 204, buttonWidth, buttonHeight}))
             {
                 SetMouseCursor(4);
                 DrawRectangle(buttonPos.x, buttonPos.y + 200, buttonWidth, buttonHeight, RAYWHITE);
-                DrawTexture(QuitButtonPressed, buttonPos.x, buttonPos.y + 204, WHITE);
+                DrawTexture(RuleButtonPressed, buttonPos.x, buttonPos.y + 204, WHITE);
+                if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                {
+                    unloadTextures();
+                    isInMainMenu = false;
+                }
+            }
+
+            //Draw the quit button
+            DrawTexture(QuitButtonUnpressed, buttonPos.x, buttonPos.y + 300, WHITE);
+            //Detect if the mouse is over the quit button
+            if(CheckCollisionPointRec(MousePos, {buttonPos.x, buttonPos.y + 304, buttonWidth, buttonHeight}))
+            {
+                SetMouseCursor(4);
+                DrawRectangle(buttonPos.x, buttonPos.y + 300, buttonWidth, buttonHeight, RAYWHITE);
+                DrawTexture(QuitButtonPressed, buttonPos.x, buttonPos.y + 304, WHITE);
                 if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
                 {
                     //Close the application
